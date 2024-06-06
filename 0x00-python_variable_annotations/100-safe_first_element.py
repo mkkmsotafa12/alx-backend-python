ae6386e4631be08dc8e858bd22c97e2bee8c0951
mkkmsotafa12 <kamalmostaf2@gmail.com>
2024-06-06 09:05:08 -0400

    10. Duck typing - first element of a sequence

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" safe first element """
+from typing import Any, Sequence, Union
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """ Return first element of list """
+    if lst:
+        return lst[0]
+    else:
+        return None
