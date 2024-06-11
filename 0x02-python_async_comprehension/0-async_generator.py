cccf82134e1f9a379832321fed4ca9e74b28f6ee
mkkmsotafa12 <kamalmostaf2@gmail.com>
2024-06-11 09:58:19 -0400

    0. Async Generator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,10 +2,10 @@
 """ A coroutine function """
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None, None]:
+async def async_generator() -> Generator[float, None, None]:
     """ Async_generator """
     for _ in range(10):
         await asyncio.sleep(1)
